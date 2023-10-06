5421462f6a875a4d024a0fea3ed45ca6cb89cca5
Jedddy <jedbalita25@gmail.com>
2023-10-06 19:24:37 +0800

    chore: create initial

---
+++ .plop/plopfile.js
@@ -0,0 +1,10 @@
+function plop(plop) {
+    // create your generators here
+    plop.setGenerator('basics', {
+        description: 'this is a skeleton plopfile',
+        prompts: [], // array of inquirer prompts
+        actions: []  // array of actions
+    });
+};
+
+module.exports = plop;
