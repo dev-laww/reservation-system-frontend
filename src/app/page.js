bd6407f2c29837b54c23f0da4cc45eaeca48a880
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-29 21:55:11 +0800

    test: removed test component

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,11 @@
-"use client"
-
-import {
-    LoginButton,
-    LogoutButton,
-    ProfileButton,
-    RegisterButton,
-} from "@components/Buttons";
+import { Navigation, Loader } from "@src/components/common";
 
 export default function Home() {
 
     return (
-        <main
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "70vh",
-            }}
-        >
-            <div>
-                <LoginButton />
-                <RegisterButton />
-                <LogoutButton />
-                <ProfileButton />
-            </div>
-        </main>
+        <>
+            <Navigation />
+            <Loader />
+        </>
     );
 }
