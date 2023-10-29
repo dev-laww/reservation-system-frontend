d0f32b5dc807334fc9ec0a4d6ef1dcdb745e0299
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-29 18:22:42 +0800

    feat: created not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,6 @@
+import { NotFound } from "@src/components";
+
+
+export default function NotFoundPage() {
+    return <NotFound />;
+}
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,36 @@
+import { Container, Title, Text, Button, Group } from "@mantine/core";
+import { Illustration } from "./Illustration";
+import classes from "./NotFound.module.css";
+
+export default function NotFound() {
+    return (
+        <Container className={classes.root}>
+            <div className={classes.inner}>
+                <Illustration className={classes.image} />
+                <div className={classes.content}>
+                    <Title className={classes.title}>Nothing to see here</Title>
+                    <Text
+                        c="dimmed"
+                        size="lg"
+                        ta="center"
+                        className={classes.description}
+                    >
+                        Page you are trying to open does not exist. You may have
+                        mistyped the address, or the page has been moved to
+                        another URL. If you think this is an error contact
+                        support.
+                    </Text>
+                    <Group justify="center">
+                        <Button
+                            component="a"
+                            href="/"
+                            size="md"
+                        >
+                            Take me back to home page
+                        </Button>
+                    </Group>
+                </div>
+            </div>
+        </Container>
+    );
+}
