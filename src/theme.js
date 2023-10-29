44e319593170968c5171ece64c2149522d0be2d7
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-29 15:35:49 +0800

    chore: code formatting

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { createTheme } from '@mantine/core';
+import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
     colors: {
-        'primary': [
+        primary: [
             "#DAC0A3",
             "#D9BF9E",
             "#D8BE99",
@@ -15,9 +15,9 @@ export const theme = createTheme({
             "#D3B97F",
             "#D2B87A",
             "#D1B775",
-            "#D0B670"
+            "#D0B670",
         ],
-        'secondary': [
+        secondary: [
             "#102C57",
             "#1A3E6E",
             "#254F85",
@@ -29,7 +29,7 @@ export const theme = createTheme({
             "#6775FD",
             "#7276FF",
         ],
-        'background': [
+        background: [
             "#F8F0E5",
             "#F7EFE0",
             "#F6EEDB",
@@ -40,43 +40,42 @@ export const theme = createTheme({
             "#F1EAC2",
             "#F0E9BD",
             "#EFE8B8",
-            "#EEE7B3"
-        ]
+            "#EEE7B3",
+        ],
     },
     headings: {
         fontFamily: "Poppins, sans-serif",
         sizes: {
             h1: {
                 fontWeight: 600,
-                fontSize: '2.25rem',
-                lineHeight: '2.75rem'
+                fontSize: "2.25rem",
+                lineHeight: "2.75rem",
             },
             h2: {
                 fontWeight: 600,
-                fontSize: '1.875rem',
-                lineHeight: '2.25rem'
+                fontSize: "1.875rem",
+                lineHeight: "2.25rem",
             },
             h3: {
                 fontWeight: 600,
-                fontSize: '1.5rem',
-                lineHeight: '1.75rem'
+                fontSize: "1.5rem",
+                lineHeight: "1.75rem",
             },
             h4: {
                 fontWeight: 600,
-                fontSize: '1.3125rem',
-                lineHeight: '1.6rem',
+                fontSize: "1.3125rem",
+                lineHeight: "1.6rem",
             },
             h5: {
                 fontWeight: 600,
-                fontSize: '1.125rem',
-                lineHeight: '1.6rem',
+                fontSize: "1.125rem",
+                lineHeight: "1.6rem",
             },
             h6: {
                 fontWeight: 600,
-                fontSize: '1rem',
-                lineHeight: '1.2rem',
+                fontSize: "1rem",
+                lineHeight: "1.2rem",
             },
-
-        }
-    }
+        },
+    },
 });
