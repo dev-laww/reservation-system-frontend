f7e999f2a4d76120c5a01f13092dc584d244242f
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-29 16:37:36 +0800

    chore: codebase cleanup

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-import { Welcome } from "../components/Welcome/Welcome";
-import { ColorSchemeToggle } from "../components/ColorSchemeToggle/ColorSchemeToggle";
+import { Welcome } from "@components/Welcome/Welcome";
+import { ColorSchemeToggle } from "@components/common/ColorSchemeToggle";
 
 export default async function HomePage() {
     return (
