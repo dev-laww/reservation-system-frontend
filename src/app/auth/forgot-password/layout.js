309467465230f63e83150451c26014cb3cbcd173
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-29 21:56:08 +0800

    feat: created forgot password route

---
+++ src/app/auth/forgot-password/layout.js
@@ -0,0 +1,10 @@
+import { Center } from "@mantine/core";
+
+export const metadata = {
+    title: "Reservation System - Forgot Password",
+    description: "Reservation System - Forgot Password",
+};
+
+export default function Layout({ children }) {
+    return children;
+}
