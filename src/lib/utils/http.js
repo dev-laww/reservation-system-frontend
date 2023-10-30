96c894714e6db08f6311282c2e41552513b37e91
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-30 19:48:59 +0800

    feat: created fetch util

---
+++ src/lib/utils/http.js
@@ -0,0 +1,38 @@
+export const fetchData = async (url, options = {}, session = null) => {
+    const response = await fetch(url, {
+        headers: {
+            ...options.headers,
+            ...(session?.accessToken && {
+                Authorization: `Bearer ${session.accessToken}`,
+            }),
+        },
+    });
+    const data = await response.json();
+
+    if (
+        response.status !== 200 &&
+        session?.refreshToken &&
+        data.detail === "Invalid token"
+    ) {
+        const newAccessToken = await fetch(
+            `${process.env.API_URL}/auth/refresh`,
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ refreshToken: session.refreshToken }),
+            }
+        );
+
+        if (newAccessToken.status === 200) {
+            const newSession = await newAccessToken.json();
+            return fetchData(url, options, {
+                ...session,
+                accessToken: newSession.accessToken,
+            });
+        }
+
+        return null;
+    }
+
+    return data;
+};
