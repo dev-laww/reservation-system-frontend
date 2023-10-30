05583aff8d4ddc606a49b0e642bae831a26dcdb8
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-30 17:56:56 +0800

    feat: added form actions

--- src/components/forms/ForgotPassword/index.js
+++ src/components/forms/ForgotPassword/index.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
     Paper,
@@ -14,9 +14,17 @@ import {
     rem,
 } from "@mantine/core";
 import { IconArrowLeft } from "@tabler/icons-react";
+import { useForm } from "@mantine/form";
 import classes from "./ForgotPassword.module.css";
 
 export default function ForgotPassword() {
+    const form = useForm({
+        initialValues: { email: "" },
+        validate: {
+            email: (val) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
+        },
+    });
+
     return (
         <Container size={460} my={30}>
             <Title className={classes.title} ta="center">
@@ -27,27 +35,56 @@ export default function ForgotPassword() {
             </Text>
 
             <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
-                <TextInput
-                    label="Your email"
-                    placeholder="your@email.com"
-                    required
-                />
-                <Group
-                    justify="space-between"
-                    mt="lg"
-                    className={classes.controls}
+                <form
+                    onSubmit={form.onSubmit(
+                        (values, _event) => console.log(values),
+                        (validationErrors, _values, _event) => {
+                            console.log(validationErrors);
+                        }
+                    )}
                 >
-                    <Anchor c="dimmed" size="sm" className={classes.control} href="/auth">
-                        <Center inline>
-                            <IconArrowLeft
-                                style={{ width: rem(12), height: rem(12) }}
-                                stroke={1.5}
-                            />
-                            <Box ml={5}>Back to the login page</Box>
-                        </Center>
-                    </Anchor>
-                    <Button className={classes.control} color="secondary">Reset password</Button>
-                </Group>
+                    <TextInput
+                        required
+                        label="Your email"
+                        placeholder="your@email.com"
+                        value={form.values.email}
+                        error={form.errors.email && "Please enter valid email"}
+                        radius="md"
+                        onChange={(event) =>
+                            form.setFieldValue(
+                                "email",
+                                event.currentTarget.value
+                            )
+                        }
+                    />
+                    <Group
+                        justify="space-between"
+                        mt="lg"
+                        className={classes.controls}
+                    >
+                        <Anchor
+                            c="dimmed"
+                            size="sm"
+                            className={classes.control}
+                            href="/auth"
+                        >
+                            <Center inline>
+                                <IconArrowLeft
+                                    style={{ width: rem(12), height: rem(12) }}
+                                    stroke={1.5}
+                                />
+                                <Box ml={5}>Back to the login page</Box>
+                            </Center>
+                        </Anchor>
+                        <Button
+                            className={classes.control}
+                            color="secondary"
+                            type="submit"
+                        >
+                            Reset password
+                        </Button>
+                    </Group>
+                </form>
             </Paper>
         </Container>
     );
