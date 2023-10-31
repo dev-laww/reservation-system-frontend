1d38624b62b8bd7015cbed752897d09832af63a4
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-31 18:37:15 +0800

    feat: finished creating payments page

--- src/app/(admin)/payments/page.js
+++ src/app/(admin)/payments/page.js
@@ -1,7 +1,25 @@
-export default function ResetPassword() {
+import { Paper } from "@mantine/core";
+import { authOptions } from "@lib/auth";
+import { getServerSession } from "next-auth";
+import { fetchData } from "@utils/http";
+import { Payments as PaymentsTable } from "@components/tables";
+import classes from "./Payments.module.css";
+
+export default async function Payments() {
+    const session = await getServerSession(authOptions);
+    const payments = await fetchData(
+        `${process.env.API_URL}/api/payments`,
+        {},
+        session
+    );
+
+    console.log(payments);
+    console.log(payments.data[0].user);
+    console.log(payments.data[0].booking);
+
     return (
-        <>
-            Payments
-        </>
+        <Paper shadow="sm" padding="md" className={classes.paper}>
+            <PaymentsTable data={payments.data} />
+        </Paper>
     );
 }
