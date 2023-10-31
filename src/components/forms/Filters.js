44be2aa9cd9e6fd48a457b6c91ad83a994e17211
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-31 16:28:02 +0800

    feat: created filters form

---
+++ src/components/forms/Filters.js
@@ -0,0 +1,211 @@
+"use client";
+
+import {
+    TextInput,
+    Select,
+    Button,
+    Grid,
+    Group,
+    Container,
+    Collapse,
+    rem,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useDisclosure } from "@mantine/hooks";
+import { useRouter } from "next/navigation";
+
+export default function Filters({ searchParams }) {
+    const form = useForm({
+        initialValues: searchParams,
+    });
+    const [opened, { toggle }] = useDisclosure(false);
+    const router = useRouter();
+
+    return (
+        <Container pt={rem(20)}>
+            <Group
+                style={{
+                    width: "100%",
+                    marginBottom: rem(20),
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    alignItems: "center",
+                }}
+            >
+                <Button
+                    type="submit"
+                    variant="light"
+                    radius="md"
+                    onClick={toggle}
+                    color="secondary"
+                >
+                    {opened ? "Hide" : "Show"} filters
+                </Button>
+            </Group>
+            <Collapse in={opened}>
+                <form
+                    onSubmit={form.onSubmit((values, _events) => {
+                        const params = new URLSearchParams(values);
+                        toggle();
+                        router.push(`/search?${params.toString()}`);
+                    })}
+                >
+                    <Grid>
+                        <Grid.Col span={{ sm: 12, md: 6, lg: 3 }}>
+                            <TextInput
+                                label="Min Price"
+                                placeholder={form.values.min_price}
+                                value={form.values.min_price}
+                                onChange={(event) =>
+                                    form.setFieldValue(
+                                        "min_price",
+                                        event.currentTarget.value
+                                    )
+                                }
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={{ sm: 12, md: 6, lg: 3 }}>
+                            <TextInput
+                                label="Max Price"
+                                placeholder={form.values.max_price}
+                                value={form.values.max_price}
+                                onChange={(event) =>
+                                    form.setFieldValue(
+                                        "max_price",
+                                        event.currentTarget.value
+                                    )
+                                }
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={{ sm: 12, md: 6, lg: 3 }}>
+                            <TextInput
+                                label="Price"
+                                placeholder={form.values.price}
+                                value={form.values.price}
+                                onChange={(event) =>
+                                    form.setFieldValue(
+                                        "price",
+                                        event.currentTarget.value
+                                    )
+                                }
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={{ sm: 12, md: 6, lg: 3 }}>
+                            <TextInput
+                                label="Min Occupancy"
+                                placeholder={form.values.min_occupancy}
+                                value={form.values.min_occupancy}
+                                onChange={(event) =>
+                                    form.setFieldValue(
+                                        "min_ccupancy",
+                                        event.currentTarget.value
+                                    )
+                                }
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={{ sm: 12, md: 6, lg: 3 }}>
+                            <TextInput
+                                label="Max Occupancy"
+                                placeholder={form.values.max_occupancy}
+                                value={form.values.max_occupancy}
+                                onChange={(event) =>
+                                    form.setFieldValue(
+                                        "max_ccupancy",
+                                        event.currentTarget.value
+                                    )
+                                }
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={{ sm: 12, md: 6, lg: 3 }}>
+                            <TextInput
+                                label="Occupancy"
+                                placeholder={form.values.occupancy}
+                                value={form.values.occupancy}
+                                onChange={(event) =>
+                                    form.setFieldValue(
+                                        "occupancy",
+                                        event.currentTarget.value
+                                    )
+                                }
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={{ sm: 12, md: 6, lg: 3 }}>
+                            <Select
+                                searchable
+                                label="Sort by"
+                                placeholder="Pick value"
+                                value={form.values.sort}
+                                onChange={(value) =>
+                                    form.setFieldValue("sort", value)
+                                }
+                                data={[
+                                    {
+                                        value: "price",
+                                        label: "Price",
+                                    },
+                                    {
+                                        value: "max_occupancy",
+                                        label: "Max Occupancy",
+                                    },
+                                ]}
+                            />
+                        </Grid.Col>
+                        <Grid.Col span={{ sm: 12, md: 6, lg: 3 }}>
+                            <Select
+                                searchable
+                                label="Order by"
+                                placeholder="Pick value"
+                                value={form.values.order}
+                                onChange={(value) =>
+                                    form.setFieldValue("order", value)
+                                }
+                                data={[
+                                    {
+                                        value: "asc",
+                                        label: "Ascending",
+                                    },
+                                    {
+                                        value: "desc",
+                                        label: "Descending",
+                                    },
+                                ]}
+                            />
+                        </Grid.Col>
+                    </Grid>
+                    <Group
+                        style={{
+                            width: "100%",
+                            marginTop: rem(20),
+                            display: "flex",
+                            justifyContent: "flex-end",
+                            alignItems: "center",
+                        }}
+                        gap={10}
+                    >
+                        <Button
+                            variant="light"
+                            radius="md"
+                            color="secondary"
+                            onClick={() => {
+                                router.push("/");
+                                form.setValues((prev) => ({}));
+                                toggle();
+                                router.refresh();
+                            }}
+                        >
+                            Reset
+                        </Button>
+                        <Button
+                            type="submit"
+                            variant="light"
+                            radius="md"
+                            color="secondary"
+                        >
+                            Apply
+                        </Button>
+                    </Group>
+                </form>
+            </Collapse>
+        </Container>
+    );
+}
--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,3 +1,4 @@
 export { default as Auth } from "./Auth";
 export { default as ForgotPassword } from "./ForgotPassword";
 export { default as ResetPassword } from "./ResetPassword";
+export { default as Filters } from "./Filters";
