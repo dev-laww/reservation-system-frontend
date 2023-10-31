c23d4dbdca7b55b8f05d58d988bda1e965a5222b
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-31 13:51:16 +0800

    feat: created tenants page

--- src/app/(admin)/tenants/page.js
+++ src/app/(admin)/tenants/page.js
@@ -1,7 +1,21 @@
-export default function ResetPassword() {
+import { fetchData } from "@utils/http";
+import { authOptions } from "@lib/auth";
+import { getServerSession } from "next-auth";
+import { Paper } from "@mantine/core";
+import { Tenants as TenantTable } from "@components/tables";
+import classes from "./Tenants.module.css";
+
+export default async function Tenants() {
+    const session = await getServerSession(authOptions);
+    const tenants = await fetchData(
+        `${process.env.API_URL}/api/tenants`,
+        {},
+        session
+    );
+
     return (
-        <>
-            Tenants
-        </>
+        <Paper shadow="sm" padding="md" className={classes.paper}>
+            <TenantTable data={tenants.data} />
+        </Paper>
     );
 }
