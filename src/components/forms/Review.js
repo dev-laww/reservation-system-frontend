86fe4a7e3a5763f41893e2aa8c229e2c92e560b4
tora-o <97511605+tora-o@users.noreply.github.com>
2023-11-02 14:32:54 +0800

    feat: add Review component with form and modal

---
+++ src/components/forms/Review.js
@@ -0,0 +1,63 @@
+"use client";
+
+import { Modal, Button, TextInput, Rating, Group } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useDisclosure } from "@mantine/hooks";
+
+export default function Review({ session }) {
+    const [opened, { open, close }] = useDisclosure(false);
+    const form = useForm({
+        initialValues: {
+            comment: "",
+            rating: 0,
+        },
+    });
+    const handleClose = () => {
+        form.reset();
+        close();
+    }
+
+    return (
+        <>
+            <Group justify="flex-end">
+
+            <Button onClick={open} color="secondary">Leave a review</Button>
+            </Group>
+            <Modal opened={opened} onClose={handleClose} title="Leave a review" centered>
+                <form
+                    onSubmit={form.onSubmit(
+                        (values, _event) => console.log(values),
+                        (validationErrors, _values, _event) => {
+                            console.log(validationErrors);
+                        }
+                    )}
+                >
+                    <Rating
+                        label="Rating"
+                        value={form.values.rating}
+                        onChange={(value) =>
+                            form.setFieldValue("rating", value)
+                        }
+                        mb="sm"
+                    />
+                    <TextInput
+                        multiline
+                        placeholder="Leave a review"
+                        label="Review"
+                        required
+                        value={form.values.comment}
+                        onChange={(event) =>
+                            form.setFieldValue(
+                                "comment",
+                                event.currentTarget.value
+                            )
+                        }
+                    />
+                    <Group mt="sm" justify="flex-end">
+                        <Button type="submit" color="secondary">Submit</Button>
+                    </Group>
+                </form>
+            </Modal>
+        </>
+    );
+}
--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -2,3 +2,4 @@ export { default as Auth } from "./Auth";
 export { default as ForgotPassword } from "./ForgotPassword";
 export { default as ResetPassword } from "./ResetPassword";
 export { default as Filters } from "./Filters";
+export { default as Review } from "./Review";
