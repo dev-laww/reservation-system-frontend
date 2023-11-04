1be9fa96f76d5f15f363fe2f373f387b155f9521
tora-o <97511605+tora-o@users.noreply.github.com>
2023-11-05 01:27:50 +0800

    feat: added styling for mantine date

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "@mantine/core/styles.css";
 import '@mantine/carousel/styles.css';
+import '@mantine/dates/styles.css';
 import React from "react";
 import { ColorSchemeScript } from "@mantine/core";
 import { Provider, Navigation, ColorSchemeToggle } from "@components/common";
